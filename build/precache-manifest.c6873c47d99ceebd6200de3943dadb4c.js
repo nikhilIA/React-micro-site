b853ce7627f473eade7ba51bc274ba9c8fb939e3
nikhilIA <nikhil.interactive@gmail.com>
2020-01-23 12:40:49 +0530

    git-igonore-file-production-build deleted
    git-igonore-file-production-build deleted

---
+++ build/precache-manifest.c6873c47d99ceebd6200de3943dadb4c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a74a8631d2a3f7f56cd17cdfd78071d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "70b945e4e9a586aa861c",
+    "url": "/static/js/2.f7731758.chunk.js"
+  },
+  {
+    "revision": "08903bb8e7062fee7a0e",
+    "url": "/static/js/main.114d8d9c.chunk.js"
+  },
+  {
+    "revision": "0d14d96c6aeeb3c5771b",
+    "url": "/static/js/runtime~main.ab569dc0.js"
+  }
+]);
